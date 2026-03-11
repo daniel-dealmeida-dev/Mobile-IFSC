d05a210291c6663578232f5d81f0c744cfc70a06
daniel-dealmeida-dev <daniel.dealmeida.dev@gmail.com>
2026-03-11 12:02:39 -0300

    Contador

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+int contador = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
+
+        TextView tv = findViewById(R.id.TextView);
+       Button b = findViewById(R.id.button);
+    b.setOnClickListener(v -> {
+        contador++;
+
+        tv.setText(Integer.toString(contador));
+    });
     }
 }
