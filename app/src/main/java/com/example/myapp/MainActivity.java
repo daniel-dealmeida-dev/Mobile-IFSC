b6a1525a3eb8054c1eff3afbd9e2352433a8d67a
daniel-dealmeida-dev <daniel.dealmeida.dev@gmail.com>
2026-03-25 12:08:21 -0300

    Primeiro exercicio

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -11,10 +12,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-int contador = 0;
+Button bntAvancar, bntVoltar;
+
+ImageView imageView;
+
+Integer imagens[] = new Integer[]{
+        R.drawable.cachorro,
+        R.drawable.cachorro,
+        R.drawable.patinho,
+        R.drawable.gardem,
+        R.drawable.happy,
+        R.drawable.porquinho
+    };
+    int posicao=0;
+
 
 
     @Override
@@ -22,17 +37,16 @@ int contador = 0;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        bntVoltar = findViewById(R.id.button);
+        bntAvancar = findViewById(R.id.buttonAvancar);
+        imageView = findViewById(R.id.imageView);
 
-
-
-        TextView tv = findViewById(R.id.TextView);
-       Button b = findViewById(R.id.button);
-    b.setOnClickListener(v -> {
-        contador++;
-
-        Random random = new Random();
-        int r = random.nextInt();
-        tv.setText(Integer.toString(r));
+        bntAvancar.setOnClickListener(v -> {
+        imageView.setImageResource(imagens[posicao]);
     });
+
     }
+
+
+
 }
