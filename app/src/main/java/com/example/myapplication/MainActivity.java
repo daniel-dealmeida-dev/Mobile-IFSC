5df3bfe1cca0d9d61c224490694529cef85c9d12
daniel.dealemdQWNdkq <daniel.dealemdQWNdkq>
2026-04-30 11:59:47 -0300

    ListView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String nomes[] = new String[]{"Daniel", "Sofia", "Prancha", "Davi", "Iran", "Fernandinho", "Borracha"};
+    ListView a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, //Contexto
+                android.R.layout.simple_list_item_1, // Recurso de layout xml
+                android.R.id.text1,//id do textView interno no layout
+                nomes //array de String DataSource
+        );
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Intent i = new Intent (getApplicationContext(),MainActivity3.class);
+
+            i.putExtra("nome", nomes[position]);
+            startActivity(i);
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+
+public class MainActivity2 {
+
+
+}
